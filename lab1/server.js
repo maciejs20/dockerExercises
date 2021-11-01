01519d991b14f037547552b0dc823db3583cdede
Maciej Szulc <mszulc@MacBook-Pro-Maciej.local>
2021-11-01 20:59:51 +0100

    v2021

--- lab1/server.js
+++ lab1/server.js
@@ -1,18 +1,25 @@
 // program listuje zmienne środowiskowe jakie otrzymuje przy uruchomieniu
 // ---------------------------------------------------------
-const os = require('os');
-const util = require('util');
+const os = require('os')
 const sprintf = require('sprintf-js').sprintf
 
-var env = process.env;
-console.log(`Running on: ${os.hostname()} [${os.type()}, ${os.arch()}]`);
-console.log("Environemnt variables:");
+const TIMEOUT_EXIT = 1000 * 60 * 5
 
-//sort env entries
-keysSorted = Object.keys(env).sort(function (a, b) {
-  return a.localeCompare(b);
+const env = process.env
+console.log(`Running on: ${os.hostname()} [${os.type()}, ${os.arch()}]`)
+console.log('Environemnt variables:')
+
+// sort env entries
+const keysSorted = Object.keys(env).sort(function (a, b) {
+  return a.localeCompare(b)
 })
-//print them out
+
+// print them out
 keysSorted.forEach(function (value) {
-  console.log(sprintf("    %20s=%s",value, env[value]));
-});
+  console.log(sprintf('    %20s=%s', value, env[value]))
+})
+console.log('Waiting for finish - timeout [ms]:' + TIMEOUT_EXIT)
+
+setTimeout(function () {
+  console.log('Program exits.')
+}, TIMEOUT_EXIT, 0)
--- lab2/server.js
+++ lab2/server.js
@@ -1,112 +1,65 @@
 // wymagane przez aplikację moduły nodejs
 // nalezy je zainstalować aby aplikacja działała poprawnie
 // ---------------------------------------------------------
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const path = require('path');
-const os = require('os');
+const http = require('http')
+const os = require('os')
+const serve = require('./serve.js')
+const { AppConfig } = require('./appConfig.js')
+const info = require('./info.js')
+const args = require('minimist')(process.argv.slice(2))
+const auth = require('http-auth')
 
+// konfiguracja programu
+let appConfig
 
 // ---- Konfiguracja programu
-// program pobiera ją ze zmiennych środowiskowych
-// lub (w razie ich braku) wykorzystuje wartości domyślne
+// flaga --mode [FILES, ENV] ustawia tryb konfiguracji
+// jesli jej nie ma zakładamy tryb ENV
 
-//directory for our files
-const filePath = process.env.FILEDIR || "files";
-//listening port
-const myPort = process.env.PORT || 8081;
-
-//obsługiwane typy plików
-let mimeTypes = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'text/javascript',
-    '.jpg': 'image/jpeg',
-    '.png': 'image/png',
-    '.ico': 'image/x-icon',
-    '.svg': 'image/svg+xml',
-    '.eot': 'appliaction/vnd.ms-fontobject',
-    '.ttf': 'aplication/font-sfnt',
-    '.txt': 'text/plain'
-};
-
-function baseName(str) {
-    //zwraca basename pliku 
-    let li = Math.max(str.lastIndexOf('/'), str.lastIndexOf('\\'));
-    return new String(str).substring(li + 1);
+if (args.hasOwnProperty('mode')) {
+  if (args.mode === 'FILES') {
+    appConfig = new AppConfig()
+  } else if (args.mode === 'ENV') {
+    appConfig = new AppConfig(process.env)
+  } else {
+    console.log(
+      'Command line error: Unknown command line switch "mode" value: ' +
+        args.mode
+    )
+    console.log('Supported modes: FILES, ENV')
+    process.exit(1)
+  }
+} else {
+  console.log('No command line switch "mode", assuming ENV mode')
+  appConfig = new AppConfig(process.env)
 }
 
-//inicjujemy serwer http
-var server = http.createServer(function (request, response) {
+// przekaz objekt konfiguracyjny do innych modułów
+info.setConfig(appConfig)
+serve.setConfig(appConfig)
 
-    //adres IP Klienta
-    var ip = request.headers['x-forwarded-for'] ||
-        request.connection.remoteAddress ||
-        request.socket.remoteAddress ||
-        request.connection.socket.remoteAddress;
+// przygotowujemy nasz serwer
+let server
+let basic
 
+// ustawiamy autentykację
+if (appConfig.authRequired) {
+  basic = auth.basic({
+    realm: 'Login'
+  }, (username, password, callback) => {
+    callback(username === appConfig.username && password === appConfig.password)
+  })
+  // inicjujemy serwer http z autentykacją
+  console.log('Activate basic auth')
+  server = http.createServer(basic.check(serve.processRequest))
+} else {
+  // inicjujemy serwer http bez autentykacji
+  server = http.createServer(serve.processRequest)
+}
 
-    //url z zapytania
-    let urlPath = url.parse(request.url).path;
-    if (urlPath === '/') urlPath = '/index.html';
-
-    //wytnij ewentualne podkatalogi
-    urlPath = baseName(urlPath);
-
-    //rozszerzenie pliku
-    urlExt = path.extname(urlPath);
-
-    //docelowa lokalizacja w systemie plików
-    targetFile = `${__dirname}/${filePath}/${urlPath}`;
-
-    console.log(`Request from: ${ip} for ${urlPath}`);
-
-    if (mimeTypes[urlExt] !== undefined) {
-        //obsługiwany typ plików
-
-
-
-        //otwieramy stream z plikiem
-        var fileStream = fs.createReadStream(targetFile);
-
-        fileStream.on('error', function (data) {
-            //blad otwarcia pliku
-            console.log("Not found:", targetFile);
-            response.writeHead(404, {
-                'Content-Type': 'text/html;charset=utf8'
-            });
-            response.write(`<strong>${urlPath}</strong>: I don't have such file.`);
-            response.end();
-        });
-
-        fileStream.on('ready', function (data) {
-            //otwarcie pliku się udało
-            console.log("Serving file:", targetFile);
-            response.writeHead(200, {
-                'Content-Type': mimeTypes[urlExt]
-            });
-        });
-        fileStream.on('data', function (data) {
-            //wysyłamy dane
-            response.write(data);
-        });
-        fileStream.on('end', function () {
-            //koniec pliku
-            response.end();
-        });
-
-    } else {
-        //nieobsługiwany typ pliku
-        response.writeHead(404, {
-            'Content-Type': 'text/html;charset=utf8'
-        });
-        response.write(`<strong>${urlExt}</strong>: I don't serve such content`);
-        response.end();
-    }
-});
-
-//uruchamiamy serwer
-console.log(`Running on: ${os.hostname()} [${os.type()}, ${os.arch()}]`);
-console.log(`Starting nodejs server on port ${myPort} serving files from "${filePath}"`);
-server.listen(myPort);
+// uruchamiamy serwer
+console.log(`Running on: ${os.hostname()} [${os.type()}, ${os.arch()}]`)
+console.log(
+  `Starting nodejs server on port ${appConfig.appPort} serving files from "${appConfig.filePath}"`
+)
+server.listen(appConfig.appPort)
